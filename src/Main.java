84f2481583bfbabe4a5c9652a8784e51a2bff224
rogasiu <wer.now1698@gmail.com>
2019-03-06 10:56:43 +0100

    pierwsze pliki

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+// psvm--> public static void main()
+// sout --> system.out.println()
+// CTRL + SHIFT + F10 --> Run
+// Każde późniejsze odpalenie tylko F10
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
